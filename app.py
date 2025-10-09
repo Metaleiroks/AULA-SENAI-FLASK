c06391046ca2f79dd7ffad12ce7392f9e0322b4f
Metaleiroks <kevynpaulo7@gmail.com>
2025-10-09 19:18:21 -0300

    att

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 #endpoint: buscar todos os alunos
-#metodo: GET
 @app.route('/buscaraluno/<int:id>', methods=['GET'])
 def buscar_alunos(id):
     return f"aluno de codigo: {id}"
 
 #endpoint: adicionar aluno
-#metodo: POST
 @app.route('/adicionaraluno', methods=['POST'])
 def adicionar_aluno():
     
@@ -18,7 +16,6 @@ def adicionar_aluno():
     return f"Aluno Cadastrado: id:{id} - nome: {nome}"
 
 #endpoint: listar alunos
-#metodo: GET
 @app.route('/listaralunos', methods=['GET'])
 def listar_alunos():
     return "Todos alunos Listados" 
