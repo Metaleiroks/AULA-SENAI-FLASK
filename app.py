d5db73ebf84f9f14cdd043cf3df431c8e8a73684
Metaleiroks <kevynpaulo7@gmail.com>
2025-10-09 21:30:06 -0300

    Finalizado

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request
 app = Flask(__name__)
 
 alunos = [
-    {"id": 1, "nome": "Kevyn"},
-    {"id": 2, "nome": "July"},
-    {"id": 3, "nome": "Aline"},
+    {"id": 1, "nome": "Kevyn","idade": 17, "endereco": "Rua Lunares, 155"},
+    {"id": 2, "nome": "July","idade": 15, "endereco": "Rua Buenos Aires, 205"},
+    {"id": 3, "nome": "Aline","idade": 42, "endereco": "Rua Satélite, 103"},
 ]
 
 #endpoint: buscar todos os alunos
@@ -16,13 +16,16 @@ def buscar_alunos(id):
             return aluno
 
     return {"mensagem": "Aluno não encontrado"}
+
 #endpoint: adicionar aluno
 @app.route('/adicionaraluno', methods=['POST'])
 def adicionar_aluno():
     
     id = request.args.get('id')
     nome = request.args.get('nome')
-    novo = {"id": id, "nome": nome}
+    idade = request.args.get('idade')
+    endereco = request.args.get('endereco')
+    novo = {"id": id, "nome": nome, "idade": idade, "endereco": endereco}
     novo["id"] = len(alunos) + 1
     alunos.append(novo)
     return alunos 
@@ -49,9 +52,20 @@ def deletar_aluno(id):
     for aluno in alunos:
         if aluno['id'] == id:
             alunos.remove(aluno)
-            return {"mensagem": "Aluno deletado com sucesso"}
-    return {"mensagem": "Aluno não encontrado"}
+            return "Aluno deletado com sucesso", alunos
+    return {"Aviso": "Aluno não encontrado"}
 
+
+#endpoint: alterar aluno
+@app.route('/alteraraluno/<int:id>', methods=['PUT'])
+def alterar_aluno(id):
+    for aluno in alunos:
+        if aluno['id'] == id:
+            deletar_aluno(id)
+            dados = request.get_json()
+            aluno.update(dados)
+            return "Aluno alterado com sucesso", alunos
+    return {"Aviso": "Aluno não encontrado"}
 
 
 if __name__ == "__main__":
