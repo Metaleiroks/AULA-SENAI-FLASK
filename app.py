331aa4be91e34dd2353499e85f63ba2c251db661
Metaleiroks <kevynpaulo7@gmail.com>
2025-10-09 21:44:58 -0300

    Finalizado

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request
 app = Flask(__name__)
 
 alunos = [
-    {"id": 1, "nome": "Kevyn","idade": 17, "endereco": "Rua Lunares, 155"},
-    {"id": 2, "nome": "July","idade": 15, "endereco": "Rua Buenos Aires, 205"},
-    {"id": 3, "nome": "Aline","idade": 42, "endereco": "Rua Satélite, 103"},
+    {"id": 1, "nome": "Kevyn","idade": 17, "end": "Rua Lunares, 155"},
+    {"id": 2, "nome": "July","idade": 15, "end": "Rua Buenos Aires, 205"},
+    {"id": 3, "nome": "Aline","idade": 42, "end": "Rua Satélite, 103"},
 ]
 
 #endpoint: buscar todos os alunos
@@ -24,8 +24,8 @@ def adicionar_aluno():
     id = request.args.get('id')
     nome = request.args.get('nome')
     idade = request.args.get('idade')
-    endereco = request.args.get('endereco')
-    novo = {"id": id, "nome": nome, "idade": idade, "endereco": endereco}
+    endereco = request.args.get('end')
+    novo = {"id": id, "nome": nome, "idade": idade, "end": endereco}
     novo["id"] = len(alunos) + 1
     alunos.append(novo)
     return alunos 
@@ -57,14 +57,14 @@ def deletar_aluno(id):
 
 
 #endpoint: alterar aluno
-@app.route('/alteraraluno/<int:id>', methods=['PUT'])
-def alterar_aluno(id):
-    for aluno in alunos:
-        if aluno['id'] == id:
-            deletar_aluno(id)
-            dados = request.get_json()
-            aluno.update(dados)
-            return "Aluno alterado com sucesso", alunos
+@app.route('/alteraraluno', methods=['PUT'])
+def alterar_aluno():
+    alterado = request.get_json()
+    for i, aluno in enumerate(alunos):
+            if aluno['id'] == alterado ["id"]:
+                alunos.pop(i)
+                alunos.append(alterado)
+                return alunos
     return {"Aviso": "Aluno não encontrado"}
 
 
